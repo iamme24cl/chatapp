e6fb0be63fa2a68d5fbb973f603795b5583fc4bf
Chandra Lama <iamme24cl@gmail.com>
2024-06-24 23:04:23 -0700

    added Register page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import "./styles.scss";
+
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Home />
     </div>
   );
 }
